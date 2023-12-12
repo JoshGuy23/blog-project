7afe1ea5daa64b2d20f30f82f048f12eee117fe5
joshua <jhecker2001@gmail.com>
2023-12-12 10:21:02 -0600

    Added ability to go to blog posts, whose data was given in the provided API for the project.

--- main.py
+++ main.py
@@ -12,5 +12,13 @@ def home():
     return render_template("index.html", posts=blog_data)
 
 
+@app.route("/post/<post_id>")
+def get_post(post_id):
+    response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+    response.raise_for_status()
+    blog_data = response.json()
+    return render_template("post.html", posts=blog_data, id=int(post_id))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
