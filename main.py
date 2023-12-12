e05bd20bc2042c3e16a948f0e8e9eb0737eaac56
joshua <jhecker2001@gmail.com>
2023-12-12 10:03:54 -0600

    Used provided blog API to add post data onto the home page.

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+    response.raise_for_status()
+    blog_data = response.json()
+    return render_template("index.html", posts=blog_data)
 
 
 if __name__ == "__main__":
-    app.run(debut=True)
+    app.run(debug=True)
